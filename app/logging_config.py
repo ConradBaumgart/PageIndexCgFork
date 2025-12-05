4007bc214c89cad6b11530bc416ecd77b939f04e
Conrad Baumgart (Capgemini) <conrad.baumgart@capgemini.com>
2025-12-05 14:22:58 +0000

    change logging back to INFO

--- app/logging_config.py
+++ app/logging_config.py
@@ -3,7 +3,7 @@ import sys
 
 # Configure root logger
 logging.basicConfig(
-    level=logging.DEBUG,  # INFO or DEBUG for more details
+    level=logging.INFO,  # INFO or DEBUG for more details
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
     handlers=[
         logging.StreamHandler(sys.stdout),  # logs to console
