dfc41fdfcb31d72c7d89b53e0182721de7342540
Conrad Baumgart (Capgemini) <conrad.baumgart@capgemini.com>
2025-12-09 17:24:15 +0000

    fix arguments of query_documents

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ import os
 from contextlib import asynccontextmanager
 from typing import Dict, List
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Query
 
 from app.logging_config import get_logger
 from app.services.list_documents import list_documents
 from app.services.upload_document import handle_upload_document
+from app.services.query_documents import handle_query_documents
 
 logger = get_logger(__name__)
 
@@ -42,12 +43,16 @@ async def list_available_documents() -> List[Dict[str, str]]:
 
 
 @app.get("/query_documents")
-async def query_documents(query: str, documents: list[str]) -> List[Dict[str, str]]:
+async def query_documents(query: str = Query(..., description="The question or search query"), documents: List[str] = Query(
+        ...,
+        description="List of document names (repeat this query param)",
+        min_items=1,
+    )
+) -> List[str]:
     """
     (Mock) Return content from documents relevant to the query.
     """
-    result = []
-    return {"uploaded": result}
+    return handle_query_documents(query, documents)
 
 
 @app.post("/upload_document")
