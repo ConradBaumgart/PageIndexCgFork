3177baa7c00f399de87c88f1b3e1cb134ab9e015
Conrad Baumgart (Capgemini) <conrad.baumgart@capgemini.com>
2025-12-10 11:14:47 +0000

    added logging to query_documents including %string,

--- app/services/query_documents.py
+++ app/services/query_documents.py
@@ -18,6 +18,7 @@ def handle_query_documents(query: str, documents: List[str]) -> List[Dict[str, A
     """
     tbd
     """
+    logger.info("Call to handle_query_documents with arguments query=%s and documents=%s", query, documents)
 
     # 1. Step: get trees according to documents
     tree_list = []
@@ -29,9 +30,11 @@ def handle_query_documents(query: str, documents: List[str]) -> List[Dict[str, A
             if doc_name in documents:
                 tree_list.append(json_path)
         except (json.JSONDecodeError, OSError):
+            logger.exception("Error when iterating through trees")
             continue
 
     logger.info("%d documents were found from %d documents requested.", len(tree_list), len(documents))
+    logger.debug("These documents were found when searching for trees: %r", tree_list)
 
     if len(tree_list) == 0:
         raise HTTPException(status_code=404, detail=f"No documents were found.")
@@ -47,14 +50,14 @@ def handle_query_documents(query: str, documents: List[str]) -> List[Dict[str, A
         with open(tree_path, "r", encoding="utf-8") as f:
             tree = json.load(f)
     except FileNotFoundError:
-        print(f"Error: File '{tree_path}' not found.")
+        logger.exception("Error: File not found.")
     except json.JSONDecodeError as e:
-        print(f"Error decoding JSON: {e}")
+        logger.exception("Error doecing JSON")
     except Exception as e:
-        print(f"Unexpected error: {e}")
+        logger.exception("Unexpected error")
 
     tree_without_text = remove_fields(tree, "text")
-    logger.debug("Tree without text will be provided: %s", tree_without_text)
+    logger.debug("Tree without text will be provided: %r", tree_without_text)
 
     # 3. Step: create prompt to select nodes
 
@@ -75,18 +78,22 @@ def handle_query_documents(query: str, documents: List[str]) -> List[Dict[str, A
     }}
     Directly return the final JSON structure. Do not output anything else.
     """
+    logger.debug("Prompt to seach for nodes: %r", search_prompt)
+
     # 4. Step: get LLM client
     llm = LLMClient()
 
     messages = []  #
     messages.append({"role": "user", "content": search_prompt})
 
+    logger.info("Llm will be called with %s", search_prompt[:100])
     tree_search_result = llm.generate(messages)
     logger.info("llm returned %s", tree_search_result.content)
 
     # query LLM
     flattened_nodes = get_nodes(tree)
-    logger.info("flattend nodes looks like %s", flattened_nodes)
+
+    logger.debug("flattend nodes looks like %r", flattened_nodes)
 
     ## transform flattened nodes into node map
     node_map = {}
@@ -99,12 +106,12 @@ def handle_query_documents(query: str, documents: List[str]) -> List[Dict[str, A
             "text": node["text"],
             "summarry": node["summary"],
         }
-    logger.info("node_map looks like %s", node_map)
+    logger.debug("node_map looks like %r", node_map)
 
     relevant_nodes = []
 
+    # Answer from LLM contains backticks to indicate a JSON file
     llm_answer = tree_search_result.content
-
     if llm_answer.startswith("```"):
         llm_answer = re.sub(r"^```(?:json)?\s*", "", llm_answer, flags=re.IGNORECASE).strip()
         llm_answer = re.sub(r"```$", "", llm_answer).strip()
@@ -112,8 +119,11 @@ def handle_query_documents(query: str, documents: List[str]) -> List[Dict[str, A
     tree_search_result_json = json.loads(llm_answer)
     for node_id in tree_search_result_json["node_list"]:
         relevant_nodes.append(node_map[node_id])
+
     logger.info("Relevant nodes are: %s", relevant_nodes)
 
-    return_value = [{"document_name": documents[0], "nodes": relevant_nodes}]
+    return_value = [
+        {"document_name": documents[0], "nodes": relevant_nodes}
+    ]  # TODO: change documents[0] cause in any case only 1 Document will be returned
 
     return return_value
